1f64632c0ad173a8c2351f94aa77fe1d04ab80e6
James Randolph <james.randolph2016@gmail.com>
2024-08-15 16:43:07 -0400

    hand detection api working with video feed

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-     <div className="absolute left-3 top-3 z-30">
+     <div className="absolute left-3 top-3 z-30 w-24">
         <HandRecognizer setHandResults={setHandResults} />
      </div>
     </main>
--- components/HandRecognizer.tsx
+++ components/HandRecognizer.tsx
@@ -1,5 +1,6 @@
 
 
+import { FilesetResolver, HandLandmarker, HandLandmarkerResult } from "@mediapipe/tasks-vision";
 import React, { useEffect, useRef } from "react";
 
 type Props = {
@@ -20,6 +21,14 @@ const HandRecognizer = ({ setHandResults }: Props) => {
     }
 
     await initVideo(videoElement);
+
+    const handLandmarker = await initModel();
+    setInterval(() => {
+        const detections = handLandmarker.detectForVideo(videoElement, Date.now());
+        processDetections(detections, setHandResults)
+    }, 1000)
+
+    await initModel();
   };
 
   return (
@@ -40,3 +49,21 @@ async function initVideo(videoElement: HTMLVideoElement) {
     videoElement.play();
   })
 }
+async function initModel() {
+    const wasm = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm");
+    const handLandmarker = HandLandmarker.createFromOptions(wasm, {
+        baseOptions: {
+            modelAssetPath: "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task",
+            delegate: 'GPU'
+        },
+        numHands: 2,
+        runningMode: "VIDEO"
+    });
+
+    return handLandmarker;
+}
+
+function processDetections(detections: HandLandmarkerResult, setHandResults: () => void) {
+    console.log(detections);
+}
+
