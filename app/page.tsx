5756a674d5132d07d1a078e2714dfdd7d14ab7bb
James Randolph <james.randolph2016@gmail.com>
2024-08-15 16:21:07 -0400

    video feed displayuing

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import HandRecognizer from "@/components/HandRecognizer";
 
 export default function Home() {
--- components/HandRecognizer.tsx
+++ components/HandRecognizer.tsx
@@ -1,13 +1,42 @@
-import React from 'react'
+
+
+import React, { useEffect, useRef } from "react";
 
 type Props = {
-    setHandResults: () => void
-}
+  setHandResults: () => void;
+};
 
-const HandRecognizer = ({setHandResults}: Props) => {
+const HandRecognizer = ({ setHandResults }: Props) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    initVideoAndModel();
+  }, []);
+
+  const initVideoAndModel = async () => {
+    const videoElement = videoRef.current;
+    if (!videoElement) {
+      return;
+    }
+
+    await initVideo(videoElement);
+  };
+
   return (
-    <div>HandRecognizer</div>
-  )
-}
+    <div>
+      <video ref={videoRef}></video>
+    </div>
+  );
+};
 
-export default HandRecognizer
+export default HandRecognizer;
+
+async function initVideo(videoElement: HTMLVideoElement) {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+  });
+  videoElement.srcObject = stream;
+  videoElement.addEventListener("loadeddata", () => {
+    videoElement.play();
+  })
+}
